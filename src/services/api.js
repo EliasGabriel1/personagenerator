35b8768f3753196d7c0635f0c65183ab2eae9568
EliasGabriel1 <elias.147682@hotmail.com>
2021-11-02 18:29:05 -0300

    update data

--- src/services/api.js
+++ src/services/api.js
@@ -26,85 +26,85 @@ export function loadLists() {
           valor: "Olhar critico de análitico",
         },
         {
+          descricao: "Qual livro e filme preferido sucessivamente?",
+          valor: "Harry potter - carros",
+        },
+        {
           descricao: "Descrição:",
           valor:
             "Gosto de gatos, amo ler, gosto de chá e meu hobie é ver revista de modas",
-        },
-        {
-          descricao: "Qual livro e filme preferido sucessivamente?",
-          valor: "Harry potter - carros",
         },
       ],
     },
     {
       id: 2,
-      title: "Jhon",
-      profissao: "Especialista Em Marketing Performance",
-      img: "https://cdn.pixabay.com/photo/2015/09/02/13/24/girl-919048_960_720.jpg",
-      cores: ["#ffc0cb", "#fff", "#000"],
+      title: "Michel",
+      profissao: "Professor de matemática",
+      img: "https://cdn.pixabay.com/photo/2019/06/29/04/36/counselor-4305394_960_720.jpg",
+      cores: ["#ffff00", "#ffa500", "#000"],
       side1: [
         {
           about: "Salário:",
-          valor: "R$ 7.580",
+          valor: "R$ 2.400",
         },
         {
           about: "Onde:",
-          valor: "Pipefaio",
+          valor: "math .io",
         },
         {
           about: "Características da profissão:",
-          valor: "Olhar analítico e crítico que busca entender pessoas",
+          valor: "Olhar matemático, rígido, e que busca compreender problemas e resolver questão com a lógica",
         },
       ],
       side2: [
         {
           descricao: "Características:",
-          valor: "Olhar critico de análitico",
+          valor: "Perfil analítico com olhar estratégico",
+        },
+        {
+          descricao: "Qual livro e filme preferido sucessivamente?",
+          valor: "Não gosto de ler, e o filme preferido é Indiana Jones",
         },
         {
           descricao: "Descrição:",
           valor:
-            "Gosto de gatos, amo ler, gosto de chá e meu hobie é ver revista de modas",
-        },
-        {
-          descricao: "Qual livro e filme preferido sucessivamente?",
-          valor: "Harry potter - carros",
+            "Gostar de kamban, jogar League of Legends e jogos estratégicos, O cotidiano é passado de acordar, beber café, e sair para o serviço sempre chegar em ponto, no almoço, come coisas simples no almoço, café da tarde gosta de tomar café, tem preferência em dar aulas por alunos de 17 anos+, pois o resultado é muito maior, anda de bicicleta, e como lugar preferido pra comer, uma pizzaria com clima razoavelmente suave, com conexão com poucos amigos, com quem tem mais intimidades. ",
         },
       ],
     },
     {
       id: 3,
-      title: "João",
-      profissao: "Especialista Em Marketing Performance",
+      title: "Anne",
+      profissao: "Bióloga",
       img: "https://cdn.pixabay.com/photo/2015/09/02/13/24/girl-919048_960_720.jpg",
-      cores: ["#ffc0cb", "#fff", "#000"],
+      cores: ["#008", "#4169e1", "#f8f8f8"],
       side1: [
         {
           about: "Salario:",
-          valor: "R$ 7.580",
+          valor: "R$ 4.058",
         },
         {
           about: "Onde:",
-          valor: "pipefaio",
+          valor: "Empresa Bionat",
         },
         {
           about: "Características da profissão:",
-          valor: "Olhar analítico e crítico que busca entender pessoas",
+          valor: "olhar suave, gosta do natural, e sente íntimo de tudo que relaciona a natureza",
         },
       ],
       side2: [
         {
           descricao: "Características:",
-          valor: "Olhar critico de análitico",
+          valor: "Perfil tranquilo, com conexão com a natureza",
+        },
+        {
+          descricao: "Qual livro e filme preferido sucessivamente?",
+          valor: "livro seleção, e gosta da saga crepúsculo",
         },
         {
           descricao: "Descrição:",
           valor:
-            "Gosto de gatos, amo ler, gosto de chá e meu hobie é ver revista de modas",
-        },
-        {
-          descricao: "Qual livro e filme preferido sucessivamente?",
-          valor: "Harry potter - carros",
+            "Gosta de fazer viagens que envolve a natureza, ama animais, e luta pelos direitos da vida dos animais. Gosta de sair com os amigos, curtir a vida, segue conceitos hippie, gosta de músicas popular nacional, no seu cotidiano sempre gosta de pegar Starbucks antes de ir pro serviço, trabalha na linha de pesquisa, com acesso diários em fóruns e google acadêmico, gosta de publicar nas redes sociais suas viagens, aonde se conecta com a natureza e se desliga do mundo, e como melhor amigo tem um gato e uma cobra.",
         },
       ],
     },
@@ -134,13 +134,13 @@ export function loadLists() {
           valor: "Olhar criativo e descontraido",
         },
         {
-          descricao: "Descrição:",
-          valor:
-            "Gosto de Star Wars",
-        },
-        {
           descricao: "Qual livro e filme preferido sucessivamente?",
           valor: "Harry potter - carros",
+        },
+        {
+          descricao: "Descrição:",
+          valor:
+            "Gosto de Star Wars, ama desenhar e gosta de pinturas",
         },
       ],
     },
@@ -157,26 +157,26 @@ export function loadLists() {
         },
         {
           about: "Onde:",
-          valor: "pipefaio",
+          valor: "Onodontoclick",
         },
         {
           about: "Características da profissão:",
-          valor: "Olhar analítico e crítico que busca entender pessoas",
+          valor: "Olhar seguro e cientificamente análitico",
         },
       ],
       side2: [
         {
           descricao: "Características:",
-          valor: "Olhar critico de análitico",
+          valor: "Perfil descontraido porém desconfiado, bem responsável e séria",
+        },
+        {
+          descricao: "Qual livro e filme preferido sucessivamente?",
+          valor: "Pequeno Principe - Capitão América: Guerra Civil",
         },
         {
           descricao: "Descrição:",
           valor:
-            "Gosto de gatos, amo ler, gosto de chá e meu hobie é ver revista de modas",
-        },
-        {
-          descricao: "Qual livro e filme preferido sucessivamente?",
-          valor: "Harry potter - carros",
+            "Gosta de sair para lugares mais suaves como shopping, restaurante e pizzaria, apesar de não possuir tempo, gosta de ler, gosta de redes sociais, porém passa pouco tempo nelas, passa a maior parte do tempo estudante e quando tem tempo gosta de sair com sua filha, maior desafio diário, é ser mãe sozinha.",
         },
       ],
     },
