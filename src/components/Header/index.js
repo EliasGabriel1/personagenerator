0c9d199cb817b6d8cdd21cafe9f8292747a60992
EliasGabriel1 <elias.147682@hotmail.com>
2021-11-02 19:50:47 -0300

    correção targe blank

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,21 +9,21 @@ export default class header extends Component {
         <nav>
           <Ul>
             <li>
-              <a href="https://github.com/EliasGabriel1/personagenerator/blob/main/README.md" target="_blank">
+              <a href="https://github.com/EliasGabriel1/personagenerator/blob/main/README.md" target="_blank" rel="noreferrer">
                 Readme
               </a>
             </li>
             <li>
-              <a href="https://github.com/EliasGabriel1" target="_blank">Github</a>
+              <a href="https://github.com/EliasGabriel1" target="_blank" rel="noreferrer">Github</a>
             </li>
             <li>
-              <a href="https://eliasgabriel1.github.io/Portfolio/" target="_blank">Portfólio</a>
+              <a href="https://eliasgabriel1.github.io/Portfolio/" target="_blank" rel="noreferrer">Portfólio</a>
             </li>
           </Ul>
         </nav>
         <h1>Gerador de Personas</h1>
         <Flex>
-          <a href="https://www.instagram.com/eliasgahh/" target="_blank">
+          <a href="https://www.instagram.com/eliasgahh/" target="_blank" rel="noreferrer">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="#000000"
@@ -35,7 +35,7 @@ export default class header extends Component {
                 <path d="M 8 3 C 5.243 3 3 5.243 3 8 L 3 16 C 3 18.757 5.243 21 8 21 L 16 21 C 18.757 21 21 18.757 21 16 L 21 8 C 21 5.243 18.757 3 16 3 L 8 3 z M 8 5 L 16 5 C 17.654 5 19 6.346 19 8 L 19 16 C 19 17.654 17.654 19 16 19 L 8 19 C 6.346 19 5 17.654 5 16 L 5 8 C 5 6.346 6.346 5 8 5 z M 17 6 A 1 1 0 0 0 16 7 A 1 1 0 0 0 17 8 A 1 1 0 0 0 18 7 A 1 1 0 0 0 17 6 z M 12 7 C 9.243 7 7 9.243 7 12 C 7 14.757 9.243 17 12 17 C 14.757 17 17 14.757 17 12 C 17 9.243 14.757 7 12 7 z M 12 9 C 13.654 9 15 10.346 15 12 C 15 13.654 13.654 15 12 15 C 10.346 15 9 13.654 9 12 C 9 10.346 10.346 9 12 9 z" />
               </svg>
           </a>
-          <a href="https://www.facebook.com/eliasgabriel.dasilva" target="_blank">
+          <a href="https://www.facebook.com/eliasgabriel.dasilva" target="_blank" rel="noreferrer">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="#000000"
@@ -47,7 +47,7 @@ export default class header extends Component {
                 <path d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h10v-9h-3v-3h3v-1.611C16,9.339,17.486,8,20.021,8 c1.214,0,1.856,0.09,2.16,0.131V11h-1.729C19.376,11,19,11.568,19,12.718V14h3.154l-0.428,3H19v9h5c1.105,0,2-0.895,2-2V6 C26,4.895,25.104,4,24,4z" />
               </svg>
           </a>
-          <a href="https://twitter.com/EliasGahh" target="_blank">
+          <a href="https://twitter.com/EliasGahh" target="_blank" rel="noreferrer">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="#000000"
@@ -59,7 +59,7 @@ export default class header extends Component {
                 <path d="M28,6.937c-0.957,0.425-1.985,0.711-3.064,0.84c1.102-0.66,1.947-1.705,2.345-2.951c-1.03,0.611-2.172,1.055-3.388,1.295 c-0.973-1.037-2.359-1.685-3.893-1.685c-2.946,0-5.334,2.389-5.334,5.334c0,0.418,0.048,0.826,0.138,1.215 c-4.433-0.222-8.363-2.346-10.995-5.574C3.351,6.199,3.088,7.115,3.088,8.094c0,1.85,0.941,3.483,2.372,4.439 c-0.874-0.028-1.697-0.268-2.416-0.667c0,0.023,0,0.044,0,0.067c0,2.585,1.838,4.741,4.279,5.23 c-0.447,0.122-0.919,0.187-1.406,0.187c-0.343,0-0.678-0.034-1.003-0.095c0.679,2.119,2.649,3.662,4.983,3.705 c-1.825,1.431-4.125,2.284-6.625,2.284c-0.43,0-0.855-0.025-1.273-0.075c2.361,1.513,5.164,2.396,8.177,2.396 c9.812,0,15.176-8.128,15.176-15.177c0-0.231-0.005-0.461-0.015-0.69C26.38,8.945,27.285,8.006,28,6.937z" />
               </svg>
           </a>
-          <a href="https://www.linkedin.com/in/elias-gabriel-437551121/" target="_blank">
+          <a href="https://www.linkedin.com/in/elias-gabriel-437551121/" target="_blank" rel="noreferrer">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="#000000"
